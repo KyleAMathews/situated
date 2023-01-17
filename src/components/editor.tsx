94ce4f3fd91438eff49dc6308bc3fa92ebfdc590
Kyle Mathews <mathews.kyle@gmail.com>
2023-01-17 11:53:11 -0800

    backout breaking changes to editor

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,36 +1,24 @@
 import 'remirror/styles/all.css'
-import './editor.css'
 
 import React from 'react'
-import { prosemirrorNodeToHtml } from 'remirror'
-import { MarkdownExtension, PlaceholderExtension } from 'remirror/extensions'
+import { AnnotationExtension, PlaceholderExtension } from 'remirror/extensions'
 import { YjsExtension } from '../remirror-yjs'
 import { Remirror, ThemeProvider, useRemirror } from '@remirror/react'
 
 const Editor = ({ provider, xmlType }): JSX.Element => {
-  const [props, setProps] = React.useState({ provider, xmlType })
-  const { manager, state, setState } = useRemirror({
+  const { manager } = useRemirror({
     extensions: [
       new PlaceholderExtension({
-        placeholder: `optional description`,
+        placeholder: `Open second tab and start to type...`,
       }),
-      new YjsExtension({ provider: props.provider, xmlType: props.xmlType }),
+      new YjsExtension({ provider, xmlType }),
     ],
     core: { excludeExtensions: [`history`] },
   })
-  // console.log(state)
-  // console.log({ html: prosemirrorNodeToHtml(state.doc) })
-
-  console.log(xmlType)
 
   return (
     <ThemeProvider>
-      <Remirror
-        state={state}
-        onChange={(parameter) => setState(parameter.state)}
-        manager={manager}
-        autoRender="end"
-      ></Remirror>
+      <Remirror manager={manager} autoFocus autoRender="end"></Remirror>
     </ThemeProvider>
   )
 }
