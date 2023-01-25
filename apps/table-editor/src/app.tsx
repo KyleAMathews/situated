1958a04cac6bc3dc2e0ce2a6af2a2117f39c1393
Kyle Mathews <mathews.kyle@gmail.com>
2023-01-25 09:36:14 -0800

    cleanups

--- apps/table-editor/src/app.tsx
+++ apps/table-editor/src/app.tsx
@@ -59,12 +59,13 @@ function useSkipper() {
   return [shouldSkip, skip] as const
 }
 
+const ROW_COUNT = 35
+
 function App() {
-  const rerender = React.useReducer(() => ({}), {})[1]
   const { rootDoc } = useYjs()
   const dataYjs = rootDoc.getArray(`data`)
   if (dataYjs.length === 0) {
-    dataYjs.push(makeData(1000))
+    dataYjs.push(makeData(ROW_COUNT))
   }
   const data = useSubscribeYjs(dataYjs)
   const usersOnline = useAwarenessStates((clients) => {
@@ -129,6 +130,11 @@ function App() {
   const table = useReactTable({
     data,
     columns,
+    initialState: {
+      pagination: {
+        pageSize: ROW_COUNT,
+      },
+    },
     defaultColumn,
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
@@ -199,72 +205,6 @@ function App() {
           })}
         </tbody>
       </table>
-      <div className="h-2" />
-      <div className="flex items-center gap-2">
-        <button
-          className="border rounded p-1"
-          onClick={() => table.setPageIndex(0)}
-          disabled={!table.getCanPreviousPage()}
-        >
-          {`<<`}
-        </button>
-        <button
-          className="border rounded p-1"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          {`<`}
-        </button>
-        <button
-          className="border rounded p-1"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          {`>`}
-        </button>
-        <button
-          className="border rounded p-1"
-          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-          disabled={!table.getCanNextPage()}
-        >
-          {`>>`}
-        </button>
-        <span className="flex items-center gap-1">
-          <div>Page</div>
-          <strong>
-            {table.getState().pagination.pageIndex + 1} of{` `}
-            {table.getPageCount()}
-          </strong>
-        </span>
-        <span className="flex items-center gap-1">
-          | Go to page:
-          <input
-            type="number"
-            defaultValue={table.getState().pagination.pageIndex + 1}
-            onChange={(e) => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0
-              table.setPageIndex(page)
-            }}
-            className="border p-1 rounded w-16"
-          />
-        </span>
-        <select
-          value={table.getState().pagination.pageSize}
-          onChange={(e) => {
-            table.setPageSize(Number(e.target.value))
-          }}
-        >
-          {[10, 20, 30, 40, 50].map((pageSize) => (
-            <option key={pageSize} value={pageSize}>
-              Show {pageSize}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>{table.getRowModel().rows.length} Rows</div>
-      <div>
-        <button onClick={() => rerender()}>Force Rerender</button>
-      </div>
     </div>
   )
 }
